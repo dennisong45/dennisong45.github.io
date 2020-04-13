2dec58f03677bdaaccc719ab630cac3c21efaa5d
unknown <STSC@LNVO-126965.ad.csueastbay.edu>
2020-04-12 19:40:07 -0700

    fancy JS

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Hello")
+
+
+function changeImage() {
+
+ 
+    if(document.getElementById('one_pic').style.backgroundImage == 'url("./images/san-fran.jpg")')
+    {
+        document.getElementById('one_pic').style.backgroundImage = 'url("./images/kl2.jpg")';
+        document.getElementById('one_pic').innerHTML = "From Malaysia";
+    }
+    else
+    {
+        document.getElementById('one_pic').style.backgroundImage = 'url("./images/san-fran.jpg")';
+        document.getElementById('one_pic').innerHTML = "Base in Bay Area";
+    }
+    
+ 
+}   
